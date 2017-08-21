1a00fc62f22785eb317476fcdc4a7fd8f7a9548a
Neeky <1721900707@qq.com>
2017-08-21 17:13:02 +0800

    scrapy.telnet 已经不推荐使用

--- scrapy/telnet.py
+++ scrapy/telnet.py
@@ -1,3 +1,6 @@
+"""
+scrapy.telnet 已经不被推荐使用，现用scrapy.extensions.telnet 代替它
+"""
 import warnings
 from scrapy.exceptions import ScrapyDeprecationWarning
 warnings.warn("Module `scrapy.telnet` is deprecated, "
